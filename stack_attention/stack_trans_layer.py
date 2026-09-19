a2533c8b9b46c7218f071b0135589f4d3bc2e302
lucidrains <lucidrains@gmail.com>
2026-09-19 10:58:08 -0700

    setup prenorm, projections to stack inputs, action logits, and then the combining of the stack reads

--- stack_attention/stack_trans_layer.py
+++ stack_attention/stack_trans_layer.py
@@ -1,8 +1,19 @@
 from __future__ import annotations
+from functools import partial
 
 import torch
-from torch.nn import Module
+from torch import nn
+from torch.nn import Module, Linear, RMSNorm
 import torch.nn.functional as F
+
+from einops import einsum, rearrange
+from einops.layers.torch import Rearrange
+
+from torch_einops_utils import pack_with_inverse
+
+# constants
+
+LinearNoBias = partial(Linear, bias = False)
 
 # helper functions
 
@@ -11,6 +22,14 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+# gumbel
+
+def log(t, eps = 1e-20):
+    return t.clamp_min(eps).log()
+
+def gumbel_noise_like(t):
+    return -log(-log(torch.rand_like(t)))
 
 # classes
 
@@ -21,7 +40,64 @@ class StackTransLayer(Module):
         self,
         dim,
         *,
-        heads = 8,
-        dim_head = 64
+        num_stacks = 4,     # heads
+        dim_stack = 16,     # dim head
+        stack_size = 24,
+        prenorm = False
     ):
         super().__init__()
+
+        self.norm = RMSNorm(dim) if prenorm else nn.Identity()
+
+        # action related
+
+        num_actions = 3 # (push, pop, noop)
+        self.to_action_logits = LinearNoBias(dim, num_stacks * num_actions)
+        self.split_action_logits = Rearrange('... (num_stacks num_actions) -> ... num_stacks num_actions', num_actions = num_actions)
+
+        # to hidden per stack for maybe push
+
+        dim_inner = num_stacks * dim_stack
+        self.to_stack_inputs = LinearNoBias(dim, dim_inner)
+        self.split_num_stacks = Rearrange('... (num_stacks dim_stack) -> ... num_stacks dim_stack', num_stacks = num_stacks)
+
+        # combine
+
+        self.combine = LinearNoBias(dim_inner, dim)
+
+    def forward(
+        self,
+        tokens,
+        stack_states = None,
+        action_temperature = 1.,
+        stochastic_action = False
+    ):
+        orig = tokens
+
+        tokens = self.norm(tokens)
+
+        tokens, inverse_pack = pack_with_inverse(tokens, '* d')
+
+        # project to stack inputs
+
+        stack_inputs = self.to_stack_inputs(tokens)
+        stack_inputs = self.split_num_stacks(stack_inputs) # (b h d)
+
+        # the actions per stack per token
+
+        action_logits = self.to_action_logits(tokens)
+
+        if stochastic_action:
+            action_logits = action_logits + gumbel_noise_like(action_logits)
+
+        actions = (action_logits / action_temperature).softmax(dim = -1)
+
+        # stack related logic (todo ...)
+
+        # combine
+
+        out = rearrange(stack_inputs, '... h d -> ... (h d)')
+
+        out = self.combine(out)
+
+        return inverse_pack(out)
--- tests/test_stack_trans_layer.py
+++ tests/test_stack_trans_layer.py
@@ -1,4 +1,18 @@
 import pytest
+param = pytest.mark.parametrize
 
-def test_stack_trans_layer:
-    assert True
+import torch
+
+@param('stochastic_action', (False, True))
+def test_stack_trans_layer(
+    stochastic_action
+):
+    from stack_attention.stack_trans_layer import StackTransLayer
+
+    tokens = torch.randn(2, 512, 256)
+
+    layer = StackTransLayer(256)
+
+    out = layer(tokens, stochastic_action = stochastic_action)
+
+    assert out.shape == tokens.shape
